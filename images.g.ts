dfcc6b2067531977c5b98690365a71c5634e9825
PRINCE20448 <150953979+PRINCE20448@users.noreply.github.com>
2023-11-15 13:12:39 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,96 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "HOLOU":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 9 9 f f f . . . . 
+. . . f f f 9 9 9 9 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 9 9 9 9 9 9 e e f . . 
+. . f e 9 f f f f f f 9 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b 8 4 4 8 b f e f f . 
+. f e e 4 1 8 d d 8 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 9 f 9 9 9 9 9 9 f 9 e . . 
+. . 9 d f 9 9 9 9 9 9 f d 9 . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+. . . . . f 8 8 8 8 f . . . . . 
+. . . . . f 8 . . 8 f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "HOLU":
+            case "anim1":return [img`
+. . . . f f f f . . . . 
+. . f f e e e e f f . . 
+. f f e e e e e e f f . 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f b 1 1 1 1 b f 4 e 
+4 d f 1 1 1 1 1 1 f d 4 
+4 4 f 6 6 6 6 6 6 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. f f f e e e e f f f . 
+f f f e e e e e e f f f 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d 4 e f e 
+f f f e 4 4 4 4 d d 4 e 
+e 4 f b 1 1 1 e d d e . 
+. . f 6 6 6 6 f e e . . 
+. . f f f f f f f . . . 
+. . f f f . . . . . . . 
+`, img`
+. . . . . . . . . . . . 
+. . . f f f f f f . . . 
+. f f f e e e e f f f . 
+f f f e e e e e e f f f 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+e f e 4 d b b d d e f . 
+e 4 d d 4 4 4 4 e f f f 
+. e d d e 1 1 1 b f 4 e 
+. . e e f 6 6 6 6 f . . 
+. . . f f f f f f f . . 
+. . . . . . . f f f . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
